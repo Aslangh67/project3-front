b9106f5d701616d611ebd12f4667e17037b73eef
Mr. A <ag.perch@gmail.com>
2020-10-30 12:13:05 -0700

    updating the url

--- client/src/Util/API/API.js
+++ client/src/Util/API/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const URL = "http://localhost:8080";
-const URL = "https://foodsavers20-back.herokuapp.com/"
+const URL = "https://foodsavers20-back.herokuapp.com"
 
 const API = {
     // User Routes __________________________________________________
